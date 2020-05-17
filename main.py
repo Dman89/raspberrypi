bc4035bdf52fae58f172f7cda55eea420e0ff1e2
Dman89 <Daniel@DanielCudney.com>
2020-05-17 11:17:02 -0700

    Adding Faren

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ DHT_PIN = 4
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+        faren = (temperature * (9/5)) + 32;
+        print("Temp={0:0.1f}F ({1:0.1f}C) Humidity={2:0.1f}%".format(faren, temperature, humidity))
     else:
         print("Sensor failure. Check wiring.");
     time.sleep(3);
