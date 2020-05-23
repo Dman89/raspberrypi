33d565351ba9a9a8483be7d152959b89e883c6a7
Jess <jess@JessMacBookPro.local>
2020-05-23 14:35:24 -0700

    RGB File

---
+++ main.py
@@ -0,0 +1,15 @@
+import board
+import adafruit_rgbled
+import time
+
+RED_LED = 17
+GREEN_LED = 27
+BLUE_LED = 22
+
+# Create a RGB LED object
+led = adafruit_rgbled.RGBLED(RED_LED, BLUE_LED, GREEN_LED)
+led.color = (255, 0, 0)
+time.sleep(3);
+led.color = (0, 255, 0)
+time.sleep(3);
+led.color = (0, 0, 255)
