e359995219a6cc0bdc4ae4ea32fd1b2c93c776c2
Jess <jess@JessMacBookPro.local>
2020-05-23 16:03:38 -0700

    try

--- components/RGB_Termo/main.py
+++ components/RGB_Termo/main.py
@@ -1,6 +1,7 @@
 from gpiozero import RGBLED
 from time import sleep
 import Adafruit_DHT
+DHT_SENSOR = Adafruit_DHT.DHT11
 
 # Configure
 DHT_PIN = 4
@@ -9,27 +10,26 @@ RGBLED_GREEN = 27
 RGBLED_BLUE = 22
 
  
-DHT_SENSOR = Adafruit_DHT.DHT11
-led = RGBLED(red = RGBLED_RED, green = RGBLED_GREEN, blue = RGBLED_BLUE)
+# led = RGBLED(red = RGBLED_RED, green = RGBLED_GREEN, blue = RGBLED_BLUE)
 
-led.color = (1, 0, 0)
-sleep(0.5)
-led.color = (0, 1, 0)
-sleep(0.5)
-led.color = (0, 0, 1)
-sleep(0.5)
-led.color = (0, 0, 0)
+# led.color = (1, 0, 0)
+# sleep(0.5)
+# led.color = (0, 1, 0)
+# sleep(0.5)
+# led.color = (0, 0, 1)
+# sleep(0.5)
+# led.color = (0, 0, 0)
 
-# led.color = (0, 1, 0)  # full green
-# sleep(1.5)
-# led.color = (1, 0, 1)  # magenta
-# sleep(1.5)
-# led.color = (1, 1, 0)  # yellow
-# sleep(1.5)
-# led.color = (0, 1, 1)  # cyan
-# sleep(1.5)
-# led.color = (1, 1, 1)  # white
-# sleep(1.5)
+# # led.color = (0, 1, 0)  # full green
+# # sleep(1.5)
+# # led.color = (1, 0, 1)  # magenta
+# # sleep(1.5)
+# # led.color = (1, 1, 0)  # yellow
+# # sleep(1.5)
+# # led.color = (0, 1, 1)  # cyan
+# # sleep(1.5)
+# # led.color = (1, 1, 1)  # white
+# # sleep(1.5)
 
 
  
@@ -38,8 +38,8 @@ while True:
     if humidity is not None and temperature is not None:
         faren = (temperature * (9/5)) + 32;
         print("Temp={0:0.1f}F ({1:0.1f}C) Humidity={2:0.1f}%".format(faren, temperature, humidity))
-        if faren >= 76:
-            led.color = (0, 1, 0)
+        # if faren >= 76:
+        #     led.color = (0, 1, 0)
     else:
         print("Sensor failure. Check wiring.");
     sleep(3);
