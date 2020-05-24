199edf778edf81b355725551172af369470adfdc
Dman89 <Daniel@DanielCudney.com>
2020-05-23 19:46:32 -0700

    Add Comma

--- pi_modules/DHT11/main.py
+++ pi_modules/DHT11/main.py
@@ -8,7 +8,7 @@ while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
         faren = (temperature * (9/5)) + 32;
-        print("Temp={0:0.1f}F ({1:0.1f}C) Humidity={2:0.1f}%".format(faren, temperature, humidity))
+        print("Temp={0:0.1f}F ({1:0.1f}C), Humidity={2:0.1f}%".format(faren, temperature, humidity))
     else:
         print("Sensor failure. Check wiring.");
     time.sleep(3);
