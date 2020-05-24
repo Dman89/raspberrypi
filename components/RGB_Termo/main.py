b93467a02c9d8ee2f8c6f8b6126e71246c32ec3c
Dman89 <Daniel@DanielCudney.com>
2020-05-23 20:15:10 -0700

    Update Logic

--- components/RGB_Termo/main.py
+++ components/RGB_Termo/main.py
@@ -10,7 +10,7 @@ RGBLED_GREEN = 27
 RGBLED_BLUE = 22
 
  
-# led = RGBLED(red = RGBLED_RED, green = RGBLED_GREEN, blue = RGBLED_BLUE)
+led = RGBLED(red = RGBLED_RED, green = RGBLED_GREEN, blue = RGBLED_BLUE)
 
 # led.color = (1, 0, 0)
 # sleep(0.5)
@@ -20,17 +20,6 @@ RGBLED_BLUE = 22
 # sleep(0.5)
 # led.color = (0, 0, 0)
 
-# # led.color = (0, 1, 0)  # full green
-# # sleep(1.5)
-# # led.color = (1, 0, 1)  # magenta
-# # sleep(1.5)
-# # led.color = (1, 1, 0)  # yellow
-# # sleep(1.5)
-# # led.color = (0, 1, 1)  # cyan
-# # sleep(1.5)
-# # led.color = (1, 1, 1)  # white
-# # sleep(1.5)
-
 
  
 while True:
@@ -39,8 +28,17 @@ while True:
     if humidity is not None and temperature is not None:
         faren = (temperature * (9/5)) + 32;
         print("Temp={0:0.1f}F ({1:0.1f}C) Humidity={2:0.1f}%".format(faren, temperature, humidity))
-        # if faren >= 76:
-        #     led.color = (0, 1, 0)
+        setColor(faren)
     else:
         print("Sensor failure. Check wiring.");
+        setColor(76)
     sleep(3);
+
+
+def setColor(faren):
+    if faren == 76:
+            led.color = (0, 1, 0)
+    else if faren >= 76:
+        led.color = (1, 0, 0)
+    else if faren <= 76:
+        led.color = (0, 0, 1)
